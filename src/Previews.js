dd767a34d52ac32b3af6f6f5fc2bc25a385948cb
Mike <n9102125@gmail.com>
2022-07-01 21:36:46 +0800

    init

---
+++ src/Previews.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import './Previews.css';
+
+export default function Previews(props) {
+  const [files, setFiles] = useState([]);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      'image/*': [],
+    },
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const thumbs = files.map((file) => (
+    <img
+      src={file.preview}
+      // style={img}
+      // Revoke data uri after image is loaded
+      onLoad={() => {
+        URL.revokeObjectURL(file.preview);
+      }}
+    />
+  ));
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+  }, []);
+
+  return (
+    <section className="container">
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <div className="container">{thumbs}</div>
+    </section>
+  );
+}
